78ae5d34573d3087ac4033c0c09547960d4c45a5
ianmcgregor <ianmcgregor1@gmail.com>
2014-12-04 00:37:20 +0000

    updated array utils

--- src/lib/array-utils.js
+++ src/lib/array-utils.js
@@ -1,8 +1,27 @@
 'use strict';
 
 var ArrayUtils = {
+    clone: function(arr) {
+        return arr.slice(0);
+    },
+    getRandom: function(arr) {
+        return arr[Math.floor(Math.random() * arr.length)];
+    },
     isArray: function(arr) {
-        return arr instanceof Array;
+        return Array.isArray ? Array.isArray(arr) : arr && arr instanceof Array;
+    },
+    nearest: function(value, arr) {
+        var index = -1,
+            least = Number.MAX_VALUE,
+            diff;
+        arr.forEach(function(item, i) {
+            diff = Math.abs(item - value);
+            if (diff < least) {
+                least = diff;
+                index = i;
+            }
+        });
+        return index;
     },
     sortNumeric: function(arr) {
         return arr.sort(function(a,b){
@@ -13,9 +32,6 @@ var ArrayUtils = {
         return arr.sort(function(){
             return Math.random() > 0.5 ? -1 : 1;
         });
-    },
-    getRandom: function(arr) {
-        return arr[Math.floor(Math.random() * arr.length)];
     }
 };
 
