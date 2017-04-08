586ebd93a74387be40855614a48dca14dcaa3a97
Ian McGregor <ian@mcgregor.codes>
2017-04-08 16:20:46 +0100

    FPS return el and allow style override

--- fps/index.js
+++ fps/index.js
@@ -1,27 +1,29 @@
-export default function fps(el) {
+export default function FPS(style = {}) {
+    let time = 0;
+    let fps = 0;
+    let currentFps = 0;
+    let averageFps = 0;
+    let ticks = 0;
+    let totalFps = 0;
+    let lastFps = 0;
+    let lastAverage = 0;
 
-    let time = 0,
-        fps = 0,
-        currentFps = 0,
-        averageFps = 0,
-        ticks = 0,
-        totalFps = 0,
-        lastFps = 0,
-        lastAverage = 0;
+    const el = document.createElement('div');
+    el.setAttribute('id', 'fps');
+    el.style.fontFamily = 'monospace';
+    el.style.position = 'absolute';
+    el.style.left = '0';
+    el.style.top = '0';
+    el.style.padding = '2px 6px';
+    el.style.zIndex = '99999';
+    el.style.background = '#000';
+    el.style.color = '#fff';
+    el.style.fontSize = '10px';
+    document.body.appendChild(el);
 
-    if (!el) {
-        el = document.createElement('div');
-        el.setAttribute('id', 'fps');
-        el.style.position = 'absolute';
-        el.style.top = '0px';
-        el.style.right = '0px';
-        el.style.padding = '2px 6px';
-        el.style.zIndex = '99999';
-        el.style.background = '#000';
-        el.style.color = '#fff';
-        el.style.fontSize = '10px';
-        document.body.appendChild(el);
-    }
+    Object.keys(style).forEach((prop) => {
+        el.style[prop] = style[prop];
+    });
 
     function report() {
         if (currentFps === lastFps && averageFps === lastAverage) {
@@ -29,7 +31,7 @@ export default function fps(el) {
         }
         lastFps = currentFps;
         lastAverage = averageFps;
-        el.innerHTML = 'FPS: ' + currentFps + '<br />AVE: ' + averageFps;
+        el.innerHTML = `FPS: ${currentFps}<br />AVE: ${averageFps}`;
     }
 
     function update(now) {
@@ -65,6 +67,7 @@ export default function fps(el) {
 
     return {
         auto,
+        el,
         update
     };
 }
