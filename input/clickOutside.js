624af87451b7f0f3a41d80f51c43e99f5076619f
Ian McGregor <ian@mcgregor.codes>
2017-04-12 23:43:46 +0100

    Make clickoutisde more flexible

--- input/clickOutside.js
+++ input/clickOutside.js
@@ -1,10 +1,22 @@
+function getTest(el) {
+    if (Array.isArray(el)) {
+        return target => el.includes(target);
+    }
+    if (typeof el === 'function') {
+        return target => el(target);
+    }
+    return target => target === el;
+}
+
 export default function clickOutside(el, fn) {
+    const test = getTest(el);
+
     function onClickOutside(event) {
         let target = event.target;
         let inside = false;
 
         while (target !== document.body) {
-            if (target === el) {
+            if (test(target)) {
                 event.stopImmediatePropagation();
                 inside = true;
                 break;
