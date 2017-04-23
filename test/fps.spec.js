ca1f49e9d4cd8a361001752e7c3f10df97d60e2d
Ian McGregor <ian@mcgregor.codes>
2017-04-23 23:51:10 +0100

    Update fps test

--- test/fps.spec.js
+++ test/fps.spec.js
@@ -3,9 +3,8 @@ import fps from '../fps';
 describe('fps', () => {
 
     it('should have update fn', () => {
-        const counter = fps();
-        expect(counter.update).to.be.a('function');
-        expect(counter.auto).to.be.a('function');
+        expect(fps.update).to.be.a('function');
+        expect(fps.auto).to.be.a('function');
     });
 
 });
